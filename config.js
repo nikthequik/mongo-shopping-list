9e6ba412c7c3d6340cae0ab165290cc582fe64c7
Nick Brown <quik.the.nick@gmail.com>
2016-09-30 19:56:08 +0000

    change to mlab db in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                            'mongodb://nikthequik:W00dst0ck!@#@ds047146.mlab.com:47146/quikstore' :
+                            'mongodb://nikthequik:W00dst0ck!@#@ds047146.mlab.com:47146/quikstore-dev');
 exports.PORT = process.env.PORT || 8080;
